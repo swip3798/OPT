5f1ea6610f2b2491b9b96099cbe01ac5a866eae0
Christian <christian.sw@hotmail.de>
2020-09-25 11:23:06 +0200

    Fixes in order to work after install

--- singletons/PDFBackend.gd
+++ singletons/PDFBackend.gd
@@ -1,21 +1,29 @@
 extends Node
 
 var thumbnail_path = "user://thumbnails/"
+var backend_path: String = ""
 
 func _ready():
 	thumbnail_path = ProjectSettings.globalize_path(thumbnail_path)
 	var dir: Directory = Directory.new()
 	dir.make_dir_recursive(thumbnail_path)
+	if OS.has_feature("standalone"):
+		backend_path = OS.get_executable_path().get_base_dir() + "\\"
+	#OS.execute(backend_path + "getdir", [], true, output, true)
+	print(backend_path)
+	print(OS.has_feature("standalone"))
 
 func merge_docs(doc_paths: Array, output_path: String) -> Array:
 	var output = []
 	doc_paths.append(output_path)
-	OS.execute("mergedoc", doc_paths, true, output, true)
+	OS.execute(backend_path + "mergedoc", doc_paths, true, output, true)
+	print(output)
 	return output
 
 func render_thumbnails(path: String) -> Array:
 	var output: Array = []
-	OS.execute("renderthumbnails", [path, thumbnail_path], true, output, true)
+	OS.execute(backend_path + "renderthumbnails", [path, thumbnail_path], true, output, true)
+	print(output)
 	return output
 
 func get_thumbnail_path() -> String:
@@ -30,7 +38,8 @@ func get_thumbnail(page_number: int) -> Texture:
 	
 func get_page_count(path: String) -> int:
 	var output: Array = []
-	OS.execute("docinfo", [path], true, output, true)
+	OS.execute(backend_path + "docinfo", [path], true, output, true)
+	print(output)
 	return int(output[0])
 
 func select_pages(path: String, page_sequence: Array, output_path: String) -> Array:
@@ -39,7 +48,8 @@ func select_pages(path: String, page_sequence: Array, output_path: String) -> Ar
 	for i in range(len(page_sequence)):
 		arguments.append(str(page_sequence[i]))
 	arguments.append(output_path)
-	OS.execute("pageselect", arguments, true, output, true)
+	OS.execute(backend_path + "pageselect", arguments, true, output, true)
+	print(output)
 	return output
 
 func cut_document(path: String, cut_point_inc: int, page_count: int, output_prefix: String):
